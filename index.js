da0a7b6bf77d6a7f058e20d6df65a63c0c1d52e3
Seva <seva.leonov@hotmail.com>
2024-08-05 15:56:09 +0700

    Add client code

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ let connections = [];
 })();
 
 //client code
-//let ws = new WebSocket("ws://localhost:8080");
+//ws = new WebSocket("ws://localhost:8080");
 //ws.onmessage = message => console.log(`Received: ${message.data}`);
 //ws.send("Hello! I'm client")
 
